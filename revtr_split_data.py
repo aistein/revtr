fbcfa9b2662f1837101a7dafd7dbe7f249c2c789
Alex Stein <astein.ny@gmail.com>
2018-11-29 21:37:40 -0500

    .

--- revtr_split_data.py
+++ revtr_split_data.py
@@ -1,7 +1,10 @@
 #!/Users/alexstein/anaconda3/bin/python
 
-# usage:
+# revtr_split_data.py
+# usage: (remote)
 # - ./revtr_split_data.py
+# usage: (server)
+# - ./revtr_split_data.py <path to VP files>
 #
 # function:
 # - create the following file hierarchy for test and training data
@@ -25,6 +28,10 @@
 # - In other words, "training" and "test" are guaranteed NOT to have the same
 #   destination IPs, but will still properly split measurements from the same prefix. 
 
+# SERVER ...
+# runtime flag indicating whether this script is being run from a local machine or directly
+# from the server where VP measurement files reside.
+SERVER = True
 
 import sys
 import os
@@ -36,8 +43,14 @@ from collections import defaultdict
 
 # globals
 mapfile = "mappings/dests_by_prefix.json"
-vpdir = "vps/"
 ddict = defaultdict(tuple) # items look like {... , dest-ip: ("train"/"test", dnet-prefix)}
+if SERVER:
+    try:
+        vpdir = sys.argv[1]
+    except IndexError:
+        raise ValueError('error: please enter <path to vpfiles dir>')
+else:
+    vpdir = "vps/"
 
 # _tag
 # given 'ip' tell me if 'ip' has been marked for "test" or for "train"
@@ -49,10 +62,10 @@ def _tag(ip):
         return None
 
 # _filterbydest
-# given 'vpfile'.csv, return pings from that file for which dest is ddict
+# given 'vpfile'.csv, return pings from that file for which dest is in ddict
 # returns 3-tuple (tag, dest-ip, [ping-hops]), where tag = ("train/test", dnet-prefix)
 def _filterbydest(vpfile):
-    print("Filtering VP: {}".format(vpfile))
+    # print("Filtering VP: {}".format(vpfile))
     with open(vpdir+vpfile, 'r') as csvfile:
         datareader = csv.reader(csvfile)
         yield from map(lambda ping: (_tag(ping[0]), ping[0], ping[1:]),
@@ -62,26 +75,27 @@ def _filterbydest(vpfile):
 # Given 'vpfile'.csv, create two new CSV files containing "train" and "test" data splits.
 # The split is based on tags assigned to each destination IP address, to be found in ddict
 def _writetask(vpfile):
-    print("Thread executing write task for VP: {}\n".format(vpfile))
+    print("Thread executing write task for VP: {}...\n".format(vpfile))
     trainfile = open("train/vp_measurements/" + vpfile, 'w')
     testfile = open("test/vp_measurements/" + vpfile, 'w')
     for ((target, dnet), dest, hops) in _filterbydest(vpfile):
         if dnet == str(None):
             continue
         if target == "train":
-            print("TRAIN <-- {},{},{}".format(dnet, dest, ','.join(hops)))
+            # print("TRAIN <-- {},{},{}".format(dnet, dest, ','.join(hops)))
             trainfile.write("{},{},{}\n".format(dnet, dest, ','.join(hops)))
         if target == "test":
-            print("TEST <-- {},{},{}".format(dnet, dest, ','.join(hops)))
+            # print("TEST <-- {},{},{}".format(dnet, dest, ','.join(hops)))
             testfile.write("{},{},{}\n".format(dnet, dest, ','.join(hops)))
     trainfile.close()
     testfile.close()
+    print("Thread completed write task for VP: {}\n".format(vpfile))
 
 # WriteSet ...
 # Spin off a _writetask thread for the given 'vpfile'
 # Returns thread handle
 def WriteSet(vpfile):
-    t = threading.Thread(target=_writetask, args=(vpfile,))
+    t = threading.Thread(target=_writetask, args=(vpfile,), name=vpfile)
     t.start()
     return t
 
@@ -96,6 +110,7 @@ def main():
             # print("Analyzing BGP-routable prefix {}\n".format(dnet))
             if len(dests) > 1:
                 index = 0
+                num_prefixes = num_prefixes + 1
                 for dest in dests:
                     # ~1/3rd of data goes in the training set
                     if index % 3 == 0:
